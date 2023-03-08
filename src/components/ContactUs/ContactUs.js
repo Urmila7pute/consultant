977618b834d8f6446b93064969816f4f6e7b360d
Urmila7pute <urmila.m.satpute1@gmail.com>
2023-03-08 16:25:06 +0530

    testing

--- src/components/ContactUs/ContactUs.js
+++ src/components/ContactUs/ContactUs.js
@@ -21,16 +21,17 @@ class ContactUs extends Component {
     event.preventDefault();
     const data= new FormData(event.target);
     console.log(Object.fromEntries(data.entries()))
+    document.getElementById("form").reset();
   }
   render() {
     return (
       <div className="formBackground">
-       <form onSubmit={this.handleSubmit} className="form">
+       <form onSubmit={this.handleSubmit} className="form" id="form">
         <h1 className="formh1"> LET'S CONNECT</h1>
         <FormInput name="name" placeholder="Name" label="Name" type="text" handleChange={this.handleChange}/>
         <FormInput name="email" placeholder="Email" label="Email" type="email"/>
 
-        <FormInput name="phone" placeholder="Phone" label="Phone" type="text"/>
+        <FormInput name="phone" placeholder="Phone" label="Phone" type="number"/>
         <FormInput name="message" placeholder="Message" label="Message" type="text"/>
         <input type="submit" value="Submit" className="submitButton"/>
 
